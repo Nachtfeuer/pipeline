b5e61547707aeb70d2631f9173e165c9232a9c9d
Thomas Lehmann <thomas.lehmann.private@gmail.com>
2017-11-30 21:20:54 +0100

    Issue #21: code coverage improved

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -3,7 +3,8 @@
 import unittest
 from hamcrest import assert_that, equal_to
 
-from spline.components.tasks import Tasks
+from spline.components.tasks import Tasks, worker
+from spline.components.bash import Bash
 from spline.components.hooks import Hooks
 from spline.pipeline import PipelineData
 
@@ -83,3 +84,50 @@ class TestTasks(unittest.TestCase):
         assert_that(len(output), equal_to(2))
         assert_that(output[0], equal_to('cleanup 123'))
         assert_that(output[1], equal_to('hello'))
+
+    def test_tags_ordered(self):
+        """Testing for filtering of tags."""
+        pipeline = FakePipeline()
+        tasks = Tasks(pipeline, parallel=False)
+
+        definition = [{'shell': {'script': '''echo hello1''', 'tags': ['first']}},
+                      {'shell': {'script': '''echo hello2''', 'tags': ['second']}}]
+
+        pipeline.data.tags = ['first']
+        result = tasks.process(definition)
+        output = [line for line in result['output'] if line.find("hello") >= 0]
+        assert_that(len(output), equal_to(1))
+        assert_that(output[0], equal_to('hello1'))
+
+        pipeline.data.tags = ['second']
+        result = tasks.process(definition)
+        output = [line for line in result['output'] if line.find("hello") >= 0]
+        assert_that(len(output), equal_to(1))
+        assert_that(output[0], equal_to('hello2'))
+
+    def test_env_ordered(self):
+        """Testing environment variables (ordered)."""
+        pipeline = FakePipeline()
+        tasks = Tasks(pipeline, parallel=False)
+
+        definition = [{'env': {'message': 'hello'}},
+                      {'shell': {'script': '''echo "1:{{env.message}}"'''}},
+                      {'shell': {'script': '''echo "2:$message"'''}}]
+        result = tasks.process(definition)
+        output = [line for line in result['output'] if line.find("hello") >= 0]
+
+        assert_that(result['success'], equal_to(True))
+        assert_that(len(output), equal_to(2))
+        assert_that(output[0], equal_to('1:hello'))
+        assert_that(output[1], equal_to('2:hello'))
+
+    def test_worker(self):
+        """Testing worker used by class Tasks for parallel execution."""
+        data = {'creator': Bash.__name__,
+                'entry': {'script': '''echo "{{model.mode}}:{{env.message}}"'''},
+                'env': {'message': 'hello'}, 'model': {'mode': 'test'}}
+        result = worker(data)
+        output = [line for line in result['output'] if line.find("hello") >= 0]
+        assert_that(result['success'], equal_to(True))
+        assert_that(len(output), equal_to(1))
+        assert_that(output[0], equal_to('test:hello'))
